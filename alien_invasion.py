5180a69750d67a52fb2748c8c2ed5d6c4efa9087
Angelo Valenciana <rigbone@Angelos-MacBook-Pro.local>
2019-12-05 10:24:39 -0700

    updated check event to move ship to the right

--- alien_invasion.py
+++ alien_invasion.py
@@ -21,7 +21,7 @@ def run_game():
     while True:
 
         # watch for keyboard and mouse events
-        gf.check_events()
+        gf.check_events(ship)
         gf.update_screen(ai_settings, screen, ship)
 
         
