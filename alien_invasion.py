f02e0e0f5702e92628167d2f90fd4b0d6b27e8cd
Angelo Valenciana <rigbone@Angelos-MacBook-Pro.local>
2019-12-05 10:18:56 -0700

    moved the screen update funciton to game function

--- alien_invasion.py
+++ alien_invasion.py
@@ -22,12 +22,7 @@ def run_game():
 
         # watch for keyboard and mouse events
         gf.check_events()
+        gf.update_screen(ai_settings, screen, ship)
 
-        # Redraw the screen during each pass through the loop
-        screen.fill(ai_settings.bg_color)
-        ship.blitme()
         
-        # make the most recently drawn screen visible
-        pygame.display.flip()
-
 run_game()
